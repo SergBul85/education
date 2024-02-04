d0b926f3ba1865289d92d19dd40b363e335f42fd
Serhii <donetsk55555@gmail.com>
2024-02-04 23:03:13 +0200

    Lesson_109

---
+++ src/lesson_109/Main.java
@@ -0,0 +1,18 @@
+package lesson_109;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] array = {0, 1, 2, 3, 4};
+        System.out.println(array[2]);
+
+        int a = 5;
+        int b = 0;
+        System.out.println(a + b);
+
+        String str = null; // ""
+        System.out.println(str.length());
+
+        System.out.println("End of programm");
+    }
+}
