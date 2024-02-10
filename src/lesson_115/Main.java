94e228c860cd239a70ba3391173492f1400dd37f
Serhii <donetsk55555@gmail.com>
2024-02-10 22:00:51 +0200

    lesson_115

---
+++ src/lesson_115/Main.java
@@ -0,0 +1,13 @@
+package lesson_115;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());
+    }
+}
