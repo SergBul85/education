de3d810013a4ca4a8778d90a27931b5d1a8a5542
Serhii <donetsk55555@gmail.com>
2024-02-18 00:48:34 +0200

    lesson_122

--- src/lesson_121/Main.java
+++ src/lesson_121/Main.java
@@ -1,6 +1,7 @@
 package lesson_121;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,12 +15,18 @@ public class Main {
 //        for (int i = 0; i < list1.size(); i++) {
 //            list1.set(i, String.valueOf(list1.get(i).length()));
 //        }
-      
+        List<Integer> list2 = list1.stream().map(x -> x.length()).collect(Collectors.toList());
+//        System.out.println(list1);
+//        System.out.println(list2);
+        // LESSON 122
 
-        List<Integer> list2 =list1.stream().map(x -> x.length()).collect(Collectors.toList());
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-        System.out.println(list1);
-        System.out.println(list2);
+        array = Arrays.stream(array).map(x -> {
+            return x * 3;
+        }).toArray();
+
+        System.out.println(Arrays.toString(array));
 
 
     }
