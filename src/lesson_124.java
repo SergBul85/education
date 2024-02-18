5e74ee1fb20af6eabff0453ee89f0fdf7af5ccfa
Serhii <donetsk55555@gmail.com>
2024-02-18 12:19:32 +0200

    lesson_124

---
+++ src/lesson_124.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class lesson_124 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+//        Arrays.stream(array).forEach(element -> System.out.print(element * 2 + " "));
+        Arrays.stream(array).forEach(System.out :: print);
+
+    }
+}
