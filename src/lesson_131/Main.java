e91e9a0ae875b07c5b5dd2fedba16a40b4d20077
Serhii <donetsk55555@gmail.com>
2024-02-20 23:46:30 +0200

    lesson_133

--- src/lesson_131/Main.java
+++ src/lesson_131/Main.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Car car1 = new Car("BMW", 250);
-        Car car2 = new Car("AUDI", 200);
-        Car car3 = new Car("MERC", 200);
-        Car car4 = new Car("ZIL", 75);
+        Car car2 = new Car("AUDI", 350);
+        Car car3 = new Car("MERC", 350);
+        Car car4 = new Car("ZIL", 200);
 
         Owner owner1 = new Owner("Jhon Connor");
         Owner owner2 = new Owner("Bruce Willis");
@@ -33,9 +33,16 @@ public class Main {
         cars.add(car3);
         cars.add(car4);
 
-        Car firstCar = cars.stream().filter(x->x.getSpeed()==200).findFirst().get();
-
+        Car firstCar = cars.stream().filter(x -> x.getSpeed() == 200).findFirst().get();
         System.out.println("firstCar - " + firstCar);
+
+// Lesson 133
+        System.out.println("Lesson 133 ----------------------------------");
+        Car minSpeedCar = cars.stream().min((x, y) -> x.getSpeed() - y.getSpeed()).get();
+        System.out.println("minSpeedCar - " + minSpeedCar);
+
+        Car maxSpeedCar = cars.stream().max((car_1, car_2) -> car_1.getSpeed() - car_2.getSpeed()).get();
+        System.out.println("maxSpeedCar - " + maxSpeedCar);
 
     }
 }
