62e86f0b6428589b1080c0d9e44a3b8e4d6ff8f3
Serhii <donetsk55555@gmail.com>
2024-03-08 19:57:45 +0200

    lesson_149

--- src/lesson_148/CarShopJapan.java
+++ src/lesson_148/CarShopJapan.java
@@ -2,13 +2,36 @@ package lesson_148;
 
 public class CarShopJapan {
     public static void main(String[] args) {
-        Car car1 = new Car(JapanCars.LEXUS, 250);
-        car1.carInfo();
+//        Car car1 = new Car(JapanCars.LEXUS, 250);
+//        car1.carInfo();
+
+//        System.out.println(JapanCars.LEXUS.getClassOfCar());
+
+        JapanCars[] array = JapanCars.values();
+        for (var car : array) {
+            System.out.print(car + " - ");
+            System.out.println(car.getClassOfCar());
+
+        }
+
     }
 }
 
 enum JapanCars {
-    HONDA, TOYOTA, SUBARU, LEXUS;
+    HONDA("middle"),
+    TOYOTA("middle"),
+    SUBARU("sport"),
+    LEXUS("luxary");
+    String classOfCar;
+
+    JapanCars(String classOfCar) {
+        this.classOfCar = classOfCar;
+    }
+
+    public String getClassOfCar() {
+        return classOfCar;
+    }
+
 }
 
 class Car {
